20c0aa62bb8eeb3e637862e019bbce940f58b179
Joao Alberto <joaoalbertosv15@gmail.com>
2023-01-14 23:55:15 -0300

    feat: adicionando readme

--- app/backend/api/settings.py
+++ app/backend/api/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
         "NAME": "verzel_desafio",
         "USER": "root",
         "PASSWORD": "123456",
-        "HOST": "127.0.0.1",
+        "HOST": "db",
         "PORT": "3306",
     }
 }
